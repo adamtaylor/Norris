5a7c0d191fa4d67b4f2056852c2dd10cdffc9fd1
adamtaylor <adamjctaylor@gmail.com>
2009-10-25 14:30:37 +0000

    added Scan Model stub

---
+++ lib/Norris/Web/Model/Scan.pm
@@ -0,0 +1,26 @@
+package Norris::Web::Model::Scan;
+
+use strict;
+use warnings;
+use parent 'Catalyst::Model';
+
+=head1 NAME
+
+Norris::Web::Model::Scan - Catalyst Model
+
+=head1 DESCRIPTION
+
+Catalyst Model.
+
+=head1 AUTHOR
+
+Adam Taylor
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ lib/Norris/Web/View/Web.pm
@@ -0,0 +1,33 @@
+package Norris::Web::View::Web;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(TEMPLATE_EXTENSION => '.tt');
+
+=head1 NAME
+
+Norris::Web::View::Web - TT View for Norris::Web
+
+=head1 DESCRIPTION
+
+TT View for Norris::Web.
+
+=head1 SEE ALSO
+
+L<Norris::Web>
+
+=head1 AUTHOR
+
+Adam Taylor
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
