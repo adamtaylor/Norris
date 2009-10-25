482ada61ce597484df92f7d81e2ad8cc2f289a46
adamtaylor <adamjctaylor@gmail.com>
2009-10-25 13:48:37 +0000

    supressed errors for users and added a scan method

--- lib/Norris/Web/Controller/Root.pm
+++ lib/Norris/Web/Controller/Root.pm
@@ -36,13 +36,31 @@ sub default :Path {
     $c->response->status(404);
 }
 
+sub scan :Local {
+    my ( $self, $c ) = @_;
+    my $url = $c->req->body_params->{url};
+    $c->stash(
+        url => $url,
+        result => $c->model('Scan')->scan($url),
+        template => 'index.tt',
+    )
+}
+
 =head2 end
 
 Attempt to render a view, if needed.
 
 =cut
 
-sub end : ActionClass('RenderView') {}
+sub end : ActionClass('RenderView') {
+    my ( $self, $c ) = @_;
+    my $errors = scalar @{$c->error};
+    if ( $errors ) {
+        $c->res->status(500);
+        $c->res->body('internal server error');
+        $c->clear_errors;
+    }
+}
 
 =head1 AUTHOR
 
